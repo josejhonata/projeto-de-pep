717af31de7f419d2f583288e2d74f77bcb6387b5
HeberthSS <baladinhagg@gmail.com>
2020-10-27 12:55:54 -0300

    novaatt

--- caminho_login_atendende.php
+++ caminho_login_atendende.php
@@ -1,5 +1,22 @@
 <?php include 'header.php'; ?>
 <div class="Formulario">
-	<h1>Login</h1>
+	<form action="login_atendente.php" method="POST">
+		<h1 class="Center">Login</h1>
+		<div class="FormularioInput">
+			<div class="FormularioInput">
+				Nome: <br>
+				<input type="text" name="username">
+			</div>
+
+			<div class="FormularioInput">
+				Password: <br>
+				<input type="password" name="password">
+			</div>
+
+			<div class="FormularioInput FormularioInput100 Center">
+				<input type="submit" value="OK">
+			</div>
+		</form>
+	</div>
 </div>
 <?php include 'fechamento.php'; ?>
