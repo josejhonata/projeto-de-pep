885ef5fd7eefa5469629820fba074407276aacd8
josejhonata <baladinhagg@hotmail.com>
2020-11-02 12:08:59 -0300

    atualizandocadastrodocliente

--- salvar_cliente.php
+++ salvar_cliente.php
@@ -1,7 +1,9 @@
 <?php
 include 'connection.php';
 session_start();
-$consulta = $pdo->prepare('INSERT INTO cliente (nome, username, password, atendente_matri) VALUES (?, ?, ?, ?)');
+$matricul = $_SESSION['matricula'];
+
+$consulta = $pdo->prepare('INSERT INTO cliente (nome, username, password, atendente_matri) VALUES (?, ?, ?, $matricul)');
 $consulta->execute([$_POST['nome'], $_POST['username'], $_POST['password'], $_POST['atendente_matri']]);
 header('location:atendente_logado.php');
 ?>
--- salvar_dados_cliente.php
+++ salvar_dados_cliente.php
@@ -3,5 +3,5 @@ include 'connection.php';
 session_start();
 $consultando = $pdo->prepare('INSERT INTO cliente_ex (id, nome_exame, cliente_id) VALUES (?, ?, ?)');
 $consultando->execute([$_POST['nome_exame'], $_POST['cliente_id'], $_SESSION['id']]);
-header('location:cliente_logado.php');
+header('location:dadoscliente.php');
 ?>
