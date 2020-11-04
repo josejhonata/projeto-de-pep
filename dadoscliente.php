ac4b31dd2e240fc9fad84a5310925409f003ba30
josejhonata <baladinhagg@hotmail.com>
2020-11-04 06:46:03 -0300

    listandodadosdocliente

--- dadoscliente.php
+++ dadoscliente.php
@@ -3,16 +3,19 @@ include 'header.php'
 include 'connection.php';
 
 
-$result_usuarios = "SELECT * FROM usuarios";
-$resultado_usuarios = mysql_query($pdo, $result_usuarios);
+$result_usuarios = "SELECT * FROM cliente";
+$resultado_usuarios = $pdo->prepare($result_usuarios);
+$resultado_usuarios-> execute();
 
-while ($row_usuario = mysql_fetch_assoc($resultado_usuarios)) {
+while ($row_usuario = $resultado_usuarios->fetch(PDO: :FETCH_ASSOC)) {
+	
 	echo "ID: ". $row_usuario['id'] . "<br>" ;
 	echo "NOME: ". $row_usuario['nome'] . "<br>" ;
 	echo "Username: ". $row_usuario['username'] . "<br>" ;
 	echo "Password: ". $row_usuario['password'] . "<br>" ;
+
 }
-<a href="atendente_logado.php">Sair</a>
-<a href="logout.php">Sair</a>
 
 ?>
+<a href="atendente_logado.php">Sair</a>
+<a href="logout.php">Sair</a>
